21acccef91532d78d393a11779c8fdf63d69fc84
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-04-23 14:48:34 -0300

    feat: adicionar validações de email e senha na função de login

--- components/js/fetchLogin.js
+++ components/js/fetchLogin.js
@@ -1,5 +1,21 @@
 import { Alert } from 'react-native';
 
+
+function validarSenha(senha) {
+    // Verifica se tem pelo menos 1 letra maiúscula e 1 caractere especial
+    const regex = /^(?=.*[A-Z])(?=.*[@$!%*?&#._-]).+$/;
+    // Tamanho mínimo de 6 caracteres
+    const tamanhoMinimo = senha.length >= 6;
+    
+    return regex.test(senha) && tamanhoMinimo;
+}
+
+function validarEmail(email) {
+    // Regex para validar o formato do email
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email); // Retorna true se o email for válido
+}
+
 export async function loginUser(email, senha) {
     try {
         const response = await fetch('http://172.16.3.231:3000/auth/login', {
@@ -12,6 +28,16 @@ export async function loginUser(email, senha) {
                 senha: senha,
             }),
         });
+        
+        if (!validarEmail(email)) {
+            alert("Por favor, insira um email válido.");
+            return;
+        }
+
+        if (!validarSenha(senha)) {
+            alert("A senha deve conter:\n- Pelo menos 1 letra maiúscula\n- Pelo menos 1 caractere especial (@$!%*?&#._-)\n- Mínimo de 6 caracteres");
+            return;
+        }
 
         if (response.ok) {
             const data = await response.json();
